361a68abde3c9cb9f75231827a60738480ab494f
Tonmetal <tonvildia@gmail.com>
2025-08-29 04:09:54 +0200

    first commit

---
+++ favorites.js
@@ -0,0 +1,13 @@
+export const favorites =[
+    {url:"https://www.facebook.com/", name: "Facebook",fav:"https://static.xx.fbcdn.net/rsrc.php/y1/r/ay1hV6OlegS.ico"},
+    {url:"https://www.instagram.com/" ,name:"Instagram", fav:"https://static.cdninstagram.com/rsrc.php/v4/yI/r/VsNE-OHk_8a.png"},
+    {url:"https://www.youtube.com//", name: "Youtube",fav:"https://www.youtube.com/s/desktop/377f632f/img/logos/favicon_144x144.png"},
+    {url:"https://www.google.com/maps", name: "Maps",fav:"//www.google.com/images/branding/product/ico/maps15_bnuw3a_32dp.ico"},
+    {url:"https://photos.google.com/u/0/?hl=es", name: "Fotos", fav:"//photos.google.com/favicon.ico"},
+    {url:"https://drive.google.com/drive/my-drive", name: "Drive",fav:"//ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png"},
+    {url:"https://chatgpt.com/?model=auto", name: "GPT",fav:"https://cdn.oaistatic.com/assets/favicon-180x180-od45eci6.webp"},
+    {url:"https://github.com/tonmetal?tab=repositories", name: "Github",fav:"https://github.githubassets.com/favicons/favicon-dark.png"},
+    {url:"https://yts.mx/", name: "Yify", fav:"https://yts.mx/assets/images/website/apple-touch-icon-152x152.png"},
+    {url:"https://rutracker.org/forum/index.php", name: "Rutracker",fav:"https://rutracker.org/favicon.ico"},
+    {url: "https://www.filmaffinity.com/es/main.html", name: "Filmaffinity",fav:"https://www.filmaffinity.com/favicon.png"},
+    {url: "https://www.hsnstore.com/", name: "Hsn",fav:"https://www.hsnstore.com/media/favicon/default/android-icon-48x48_2.png"}]
---
+++ index.js
@@ -0,0 +1,78 @@
+import { favorites } from "./favorites.js"
+
+const topic = ["hacker", "food", "architecture", "weightlifting", "sunset", "gotic", "code", "monuments", "landscapes"]
+
+
+fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=${topic[Math.floor(Math.random()*9 )]}`)
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `linear-gradient(to top, rgba(0,0,0,0.6)0%, rgba(0,0,0,0.2) 10%, rgba(0,0,0,0)20%),
+    linear-gradient(to bottom, rgba(0,0,0,0.8)0%, rgba(0,0,0,0.2) 30%, rgba(0,0,0,0)50%), url(${data.urls.regular})`
+		document.getElementById("author").innerHTML =  data.location.name
+    })
+    .catch(err => {
+        // Use a default background image/author
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+		document.getElementById("author").textContent = `By: Dodi Achmad`
+    })
+
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+        console.log(data)
+        document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.thumb} />
+            <span>${data.name}</span>
+        `
+        document.getElementById("crypto").innerHTML += `
+            <p>${data.market_data.current_price.usd.toLocaleString("es")}$</p>
+            <p id="red">${(data.market_data.price_change_percentage_24h).toFixed(2)}%</p>
+        `
+        if(data.market_data.price_change_percentage_24h > 0){
+            document.getElementById("red").style.color = " rgb(3, 226, 3)"
+        }else{
+            
+        }
+
+    })
+    .catch(err => console.error(err))
+
+function getCurrentTime() {
+    const date = new Date()
+    document.getElementById("time").textContent = date.toLocaleTimeString("es", {timeStyle: "short"})
+}
+
+setInterval(getCurrentTime, 1000)
+
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
+            `
+        })
+        .catch(err => console.error(err))
+});
+
+let favorito = ""
+for( let favorite of favorites){ 
+    favorito += `
+    <a href="${favorite.url}"><img class="fav" src="${favorite.fav}">${favorite.name}</a>`  
+}
+document.getElementById("favorites").innerHTML = favorito
+
